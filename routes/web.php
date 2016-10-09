a11f8f7eca906feb9de7e005a2ed63f6af867b4c
Justin B <contact@machineelf.design>
2016-10-09 00:23:15 -0400

    Main Routes configuration

--- routes/web.php
+++ routes/web.php
@@ -11,18 +11,49 @@
 |
 */
 
-Route::get('/text', function () {
-    return view('booktext');
-});
+/* - - - - - - - - - - - - - - - - - - - - - - -
+Single Book and Book Text Pages
+- - - - - - - - - - - - - - - - - - - - - - - - */
 
-Route::get('/entry', function () {
-    return view('bookentry');
-});
+/* Single Bookes NOTE: maybe seperate these from the other routes */
 
-Route::post('/submit', function () {
-    return view('submit');
-});
+// Book One....
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('library/books/titles/a-thousand-years-of-yesterdays', 'PagesController@getBookSingle');
+
+Route::get('library/books/titles/a-thousand-years-of-yesterdays/text', 'PagesController@getBookText');
+
+// Book Two...
+
+Route::get('library/books/titles/essays-of-a-modern-mystic', 'PagesController@getBookSingle');
+
+Route::get('library/books/titles/essays-of-a-modern-mystic/text', 'PagesController@getBookText');
+
+/* - - - - - - - - - - - - - - -
+Main Pages and Childs
+- - - - - - - - - - - - - - - - - */
+
+/* Book Titles Index Page */
+Route::get('library/books/titles', 'PagesController@getBooksTitles');
+
+/* New Books Index Page */
+Route::get('library/books/new', 'PagesController@getBooksNew');
+
+/* Book Genres Index Page */
+Route::get('library/books/genre', 'PagesController@getBooksGenre');
+
+/* All Books Index Page */
+ /* NOTE: Change back to 'all' */
+Route::get('library/books/alls', 'PagesController@getBooksAll');
+
+/* Books Index Page */
+Route::get('library/books', 'PagesController@getBooks');
+
+/* Admin Form Page */
+Route::get('/admin/forms', 'PagesController@getAdminForms');
+
+/* Admin Page */
+Route::get('/admin', 'PagesController@getAdmin');
+
+/* Home Page */
+Route::get('/', 'PagesController@getIndex');
