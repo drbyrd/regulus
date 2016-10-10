b4bf9427495b5f1a16990a0307c265f0e8dff192
Justin B <contact@machineelf.design>
2016-10-09 22:01:18 -0400

    single book text main blade

--- resources/views/main-five.blade.php
+++ resources/views/main-five.blade.php
@@ -1,164 +1,276 @@
-
-
 <!DOCTYPE html>
-
+<!--[if lt IE 7 ]> <html lang="en" class="ie6 ielt8"> <![endif]-->
+<!--[if IE 7 ]>    <html lang="en" class="ie7 ielt8"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en" class="ie8"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!-->
 <html class="no-js" dir="ltr" lang="en">
+<!--<![endif]-->
 <head>
-    <meta charset="utf-8">
-    <meta content="ie=edge" http-equiv="x-ua-compatible">
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+	<meta charset="utf-8">
+	<meta content="ie=edge" http-equiv="x-ua-compatible">
+	<meta content="width=device-width, initial-scale=1" name="viewport">
+	<title>Regulus Reader | Welcome</title>
+	<link href="../../../../css/vendor/bootstrap.min.css" rel="stylesheet">
+	<link href="../../../../css/vendor/font-awesome.min.css" rel="stylesheet">
+	<link href="../../../../css/apps.css" rel="stylesheet">
+	<script src="../../../../js/vendor/jquery.min.js">
+	</script>
+	<script src="../../../../js/vendor/bootstrap.min.js">
+	</script>
+	<script src="../../../../js/custom.js">
+	</script><!-- Byrd -->
 
-    <title>Regulus Reader | Welcome</title>
-    <link href="../../../../css/apps.css" rel="stylesheet">
-    <link href="../../../../css/font-awesome.min.css" rel=
-    "stylesheet">
-    	<script src="../../../../js/jquery.js"></script>
-      <script src="../../../../js/custom.js"></script>
-
+	<style>
+	</style>
 </head>
-
 <body>
+	<div class="globalWrapper">
+		<div id="tableOfContents">
+			<h2><a href="#" id="toc">Table of Contents</a></h2>
+			<div class="sectionsWrapper closed">
+				<dl class="sections">
+					<dt>
+						<a class="subsection" href="#">1. Definitions</a>
+					</dt>
+					<dd>
+						<div class="subsectionContent">
+							<dl>
+								<dd>
+									<a data="" href="#sec11">1.1. What is design?</a>
+								</dd>
+								<dd>
+									<a data="" href="#sec12">1.2. What is good design?</a>
+								</dd>
+								<dd>
+									<a data="" href="#sec13">1.3. Ten principles of good design</a>
+								</dd>
+								<dd>
+									<a data="" href="#sec14">1.4. An example</a>
+								</dd>
+								<dd>
+									<a data="" href="#sec15">1.5. Video: <i>Objectified</i> trailer</a>
+								</dd>
+								<dd>
+									<a data="" href="#sec16">1.6. Is your product well-designed?</a>
+								</dd>
+							</dl>
+						</div>
+					</dd>
+					<dt>
+						<a class="subsection" href="#">2. Different kinds of design</a>
+					</dt>
+					<dd>
+						<div class="subsectionContent">
+							<dl>
+								<dd>
+									<a data="" href="#sec21">2.1. Graphic Design</a>
+								</dd>
+								<dd>
+									<a data="" href="#sec22">2.2. Interaction Design</a>
+								</dd>
+								<dd>
+									<a data="" href="#sec23">2.3. Industrial Design</a>
+								</dd>
+							</dl>
+						</div>
+					</dd>
+					<dt>
+						<a class="subsection" href="#">3. Data: what does the industry think?</a>
+					</dt>
+					<dd>
+						<div class="subsectionContent">
+							<dl>
+								<dd>
+									<a data="" href="#sec31">3.1. What qualities of design do startups<br>
+									consider most important?</a>
+								</dd>
+								<dd>
+									<a data="" href="#sec32">3.2. How important is product design?</a>
+								</dd>
+								<dd>
+									<a data="" href="#sec33">3.3. How deep does design go?</a>
+								</dd>
+							</dl>
+						</div>
+					</dd>
+					<dt>
+						<a class="subsection" href="#">4. Designer Founders: the missing link</a>
+					</dt>
+					<dd>
+						<div class="subsectionContent">
+							<dl>
+								<dd>
+									<a data="" href="#sec41">4.1. Startups + designers = ?</a>
+								</dd>
+								<dd>
+									<a data="" href="#sec42">4.2. Meet the Designer Founders</a>
+								</dd>
+								<dd>
+									<a data="" href="#sec48">4.8. Changing the world</a>
+								</dd>
+							</dl>
+						</div>
+					</dd>
+					<dt>
+						<a class="subsection" href="#">5. Finding great talent</a>
+					</dt>
+					<dd>
+						<div class="subsectionContent">
+							<dl>
+								<dd>
+									<a data="" href="#sec51">5.1. Okay, I need a designer.</a>
+								</dd>
+								<dd>
+									<a data="" href="#sec52">5.2. Go Where the designers are</a>
+								</dd>
+								<dd>
+									<a data="" href="#sec54">5.3. Resources</a>
+								</dd>
+							</dl>
+						</div>
+					</dd>
+					<dt>
+						<a class="subsection" href="#">About me & Acknowledgements</a>
+					</dt>
+					<dd>
+						<div class="subsectionContent">
+							<dl>
+								<dd>
+									<a data="" href="#section6">6. About me</a>
+								</dd>
+								<dd>
+									<a data="" href="#section7">7. Acknowledgements</a>
+								</dd>
+							</dl>
+						</div>
+					</dd>
+				</dl><!-- / sections -->
+			</div><!-- / sectionsWrapper -->
+		</div><!-- / tableOfContents -->
+	</div>
+	<div class="account">
+		<ul>
+			<li class="ripple">Sign Out</li>
+			<li class="ripple">Switch account</li>
+			<li class="ripple">Send feedback</li>
+			<li class="ripple">Help</li>
+		</ul>
+	</div>
+	<div class="side-menu">
+		<div class="menu-holder">
+			<button class="menu"></button>
+			<p>Regulus <span>Reader</span></p>
+		</div>
+		<ul>
+			<li class="active">
+				<a href="./../../../../">Home</a>
+			</li>
+			<li>
+				<a href="./../../../../library/books/alls">Browse Books</a>
+			</li>
+			<li>Brows Podcasts</li>
+			<li>Browse Video</li>
+			<li>Podcasts</li>
+			<li>Browse Stations</li>
+			<li>Shop</li>
+		</ul><button class="cta" type="button">Subscribe Now</button>
+	</div>
+	<div class="side-menu-overlay"></div>
+	<header>
+		<button class="menu ripple"></button>
+		<h1>All Books</h1><input class="desktop-only" id="input" placeholder="Search" type="text"> <label class="mobile-only ripple" for="input"></label>
+		<div class="profile"></div><!--
+      <ul>
+          <li class="ripple">
+              <a href="../all/">All</a>
+          </li>
 
-    <!-- - - - - - - - - - - - - - - - - - - -
-  OFF CANVAS MENUS
-  - - - - - - - - - - - - - - - - - - - - - -->
-    <!-- Profile dropdown -->
+
+          <li class="ripple">
+              <a href="../new/">New</a>
+          </li>
 
 
-    <div class="account">
-        <ul>
-            <li class="ripple">Sign Out</li>
+          <li class="ripple">
+              <a href="../genre/">Genre</a>
+          </li>
 
 
-            <li class="ripple">Switch account</li>
+          <li class="ripple">
+              <a href="../../topic/">Topics</a>
+          </li>
 
 
-            <li class="ripple">Send feedback</li>
-
-
-            <li class="ripple">Help</li>
-        </ul>
-    </div>
-    <!-- //
-      Profile dropdown -->
-    <!-- left side off canvas men -->
-
-
-    <div class="side-menu">
-        <div class="menu-holder">
-            <button class="menu"></button>
-
-            <p>Regulus <span>Reader</span></p>
-        </div>
-
-
-        <ul>
-            <li class="active">
-                <a href="../../../../">Home</a>
-            </li>
-
-
-            <li>
-                <a href="../../">Browse Books</a>
-            </li>
-
-
-            <li>Brows Podcasts</li>
-
-
-            <li>Browse Video</li>
-
-
-            <li>Podcasts</li>
-
-
-            <li>Browse Stations</li>
-
-
-            <li>Shop</li>
-        </ul>
-        <button class="cta" type="button">Subscribe Now</button>
-    </div>
-    <!-- //
-      left side off canvas men -->
-    <!-- Off canvas menu overlay -->
-
-
-    <div class="side-menu-overlay">
-    </div>
-    <!-- //
-    Off canvas menu overlay -->
-    <!-- - - - - - - - - - - - - - - - - - - -
-    HEADER
-    - - - - - - - - - - - - - - - - - - - - - -->
-
-
-    <header>
-        <!-- Header Hamberger -->
-        <button class="menu ripple"></button> <!-- //
-        Header Hamberger -->
-         <!-- Header Title -->
-
-
-        <h1>All Books</h1>
-        <input class="desktop-only" id="input" placeholder="Search" type=
-        "text"> <label class="mobile-only ripple" for="input"></label> <!-- //
-        Header Title -->
-         <!-- Profile Avatar -->
-
-
-        <div class="profile">
-        </div>
-        <!-- //
-        Profile Avatar -->
-        <!-- Secondary Hearder Menu
-
-
-        <ul>
-            <li class="ripple">
-                <a href="../all/">All</a>
-            </li>
-
-
-            <li class="ripple">
-                <a href="../new/">New</a>
-            </li>
-
-
-            <li class="ripple">
-                <a href="../genre/">Genre</a>
-            </li>
-
-
-            <li class="ripple">
-                <a href="../../topic/">Topics</a>
-            </li>
-
-
-            <li style="list-style: none; display: inline">
-                <div class="slider">
-                </div>
-            </li>
-        </ul> -->
-        <!--//
-        Secondary Hearder Menu -->
-    </header>
-    <!-- //
-    HEADER -->
-    <!-- - - - - - - - - - - - - - - - - - - -
-    MAIN CONTENT
-    - - - - - - - - - - - - - - - - - - - - - -->
-
-
-    <main>
-        <!-- Main Wrapper -->
-
-@yield('content')
-
-        <!-- //
+          <li style="list-style: none; display: inline">
+              <div class="slider">
+              </div>
+          </li>
+      </ul>
+    -->
+	</header><!-- MOBILE NAVIGATION -->
+	<!-- MAIN SECTION -->
+	<main>
+		@yield('content') <!-- //
           Main Wrapper -->
-    </main>
-    <!-- //
-        MAIN CONTENT -->
+	</main><!-- FOOTER -->
+	<script>
+	     /*
+	     * Wanted to recreate a progress indicator of a [journalism website](https://decorrespondent.nl/en "online journalism platform")
+	     * It's basically a fork of [this thing here on CodePen](http://codepen.io/thathurtabit/pen/ymECf "")
+	     * [tutorial on ghosthorses.co.uk](http://www.ghosthorses.co.uk/production-diary/super-simple-responsive-progress-bar/ "")
+	     * Simply test this script, by clicking the top-left button; add more paragraphs in the article: it should work, no matter how long or short the article.
+	     * On mobile please try http://bramdeh.home.xs4all.nl/playground/ui-patterns/reading-progress-bar/ (CodePen's iFrame conflict)
+	     */
+	     $(function() {
+	       $("body").prepend("<div class='fixed'><div class='progress-wrap' data-progress-percent='0'><div class='progress-bar'><\/div><\/div><\/div>");
+	       });
+	     $( "#addP" ).click(function() {
+	     $( "article" ).prepend("<p class='new-paragraph'>New paragraph: adipisicing quasi natus dignissimos veritatis quasi. Nesciunt aliquam harum distinctio nobis modi dolorem incidunt tempora repudiandae harum. Voluptas eum harum mollitia officia non magni quos! Repellat architecto similique fuga aliquid.<\/p>" );
+	     });
+
+	     // on page load...
+	     //
+	     $(window).scroll(function () {
+
+	     moveProgressBar();
+	     // on browser resize...
+	     $(window).resize(function() {
+	       moveProgressBar();
+	     });
+
+	     // SIGNATURE PROGRESS
+	     function moveProgressBar() {
+	       console.log("moveProgressBar");
+	       var getPercent = ($('.progress-wrap').data('progress-percent') / 100);
+	       var getProgressWrapWidth = $('.progress-wrap').width();
+
+	       var zeroHeight = jQuery(window).height() // get minimal amount of scroll
+	     if ($(window).scrollTop() < zeroHeight) {
+	       var progressTotal = getPercent * getProgressWrapWidth;
+
+	       var animationLength = 500;
+
+	     }
+
+	       var tenthHeight = jQuery(window).height() / 10 // Splits screen in ten parts, to get at least 1/10 of window height
+	         if ($(window).scrollTop() > tenthHeight) {
+
+	           var calcDivider =  ( jQuery(document).height() - jQuery(window).height() ) / 532;
+
+	           var progressTotal = (getPercent * getProgressWrapWidth  + $ (window).scrollTop()) / calcDivider;
+
+	           var animationLength = 500;
+	         }
+
+	       // on page load, animate percentage bar to data percentage length
+	       // .stop() used to prevent animation queueing
+	       $('.progress-bar').stop().animate({
+	         left: progressTotal
+	       }, animationLength);
+	     }
+
+	     })
+
+	</script>
 </body>
 </html>
